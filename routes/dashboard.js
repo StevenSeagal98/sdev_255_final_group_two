5986ad4ba8d063cb1778d670ebdf343d3e38dca0
pkualkat <pkualkat@ivytech.edu>
2024-03-08 17:06:29 -0500

    ds

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,6 +2,8 @@ const { get } = require('../controllers/dashboard')
 const dashboardRouter = require('express').Router()
 
 dashboardRouter.get('/', get)
+router.get('/enroll', (req, res) => res.render('enroll'))
+router.use('/dropCourse', require('./dropCourse'))
 
 module.exports = dashboardRouter
 
