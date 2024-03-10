094dba0f7567962acba0dc7c8320b860541568dc
nateorona98 <nathaniel.orona98@gmail.com>
2024-03-10 12:08:51 -0500

    updates from merge

--- routes/cart.js
+++ routes/cart.js
@@ -1,6 +1,7 @@
-const { get } = require('../controllers/cart');
+const { get, post } = require('../controllers/cart');
 const cartRouter = require('express').Router();
 
-cartRouter.get('/', get);
+cartRouter.get('/', get)
+cartRouter.post('/', post)
 
-module.exports = cartRouter;
+module.exports = cartRouter
